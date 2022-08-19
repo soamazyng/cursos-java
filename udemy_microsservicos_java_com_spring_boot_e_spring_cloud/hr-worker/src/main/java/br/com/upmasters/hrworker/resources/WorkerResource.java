bfdf0c4d4d5443f4c4860df9bbe040c2ccbe129f
Jay Benedicto <jaybenedicto@MacBook-Pro-de-Jay.local>
2022-08-18 21:21:10 -0300

    hrworker implementation

---
+++ udemy_microsservicos_java_com_spring_boot_e_spring_cloud/hr-worker/src/main/java/br/com/upmasters/hrworker/resources/WorkerResource.java
@@ -0,0 +1,30 @@
+package br.com.upmasters.hrworker.resources;
+
+import br.com.upmasters.hrworker.entities.Worker;
+import br.com.upmasters.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/workers")
+public class WorkerResource {
+
+  @Autowired
+  private WorkerRepository repository;
+
+  @GetMapping()
+  public ResponseEntity<List<Worker>> findAll(){
+    List<Worker> list = repository.findAll();
+    return ResponseEntity.ok(list);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Worker> findById(@PathVariable Long id){
+    Worker worker = repository.findById(id).get();
+    return ResponseEntity.ok(worker);
+  }
+
+}
