7ee59965559ce0276765214292610780914a14d2
Jaqueline Benedicto <soamazing@gmail.com>
2023-10-29 08:23:56 -0300

    alga

---
+++ algaworks/algafood-api/src/main/java/com/upmasters/algafood/controllers/MeuPrimeiroController.java
@@ -0,0 +1,14 @@
+package com.upmasters.algafood.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MeuPrimeiroController {
+  @GetMapping("/hello")
+  @ResponseBody
+  public String hello() {
+        return "Hello!123";
+    }
+}
